795373e0359fcddaa972fe1051f8287fa26b8f52
Your Name <you@example.com>
2023-09-21 19:43:50 +0300

    spring boot

---
+++ springBoot/demo/demo/src/main/java/com/example/demo/controller/PatientController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.PrivateKey;
+
+@RestController
+
+public class PatientController {
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping("/print-name")
+    public String printPatientName(){
+        return PatientService.printName("Sara");
+    }
+}
